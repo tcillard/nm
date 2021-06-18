31c30ed1a17c03764c2ad5566ebe47691832bc77
tcillard <tcillard@student.42.fr>
2021-06-18 14:51:32 +0200

    maj makefile, struct

--- includes/nm.h
+++ includes/nm.h
@@ -6,28 +6,37 @@
 /*   By: tcillard <tcillard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 20:47:55 by tcillard          #+#    #+#             */
-/*   Updated: 2021/06/16 22:35:11 by tcillard         ###   ########.fr       */
+/*   Updated: 2021/06/17 21:36:58 by tcillard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 # define FT_LS_H
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <elf.h>
-#include <stdio.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/mman.h>
+# include <elf.h>
+# include <stdio.h>
+
+# define B32    1
+# define B64    2
+
+typedef enum
+{
+    B32,
+    B64,
+}       e_arch;
 
 typedef struct  s_elf
 {
     Elf32_Ehdr  *fileContent32;
     Elf64_Ehdr  *fileContent64;
     size_t      size;
-    int         arch;
+    e_arch      arch;
 }               t_elf;
 
-void    printStruct(t_elf elf);
+void    printStruct(t_elf *elf);
 #endif
--- sources/nm.c
+++ sources/nm.c
@@ -12,7 +12,7 @@ t_elf   *initElfStruct() {
     elf = getFileStruct();
     elf->arch = 0;
     elf->fileContent32 = NULL;
-    elf->fileContent32 = NULL;
+    elf->fileContent64 = NULL;
     elf->size = 0;
     return elf;
 }
@@ -21,6 +21,7 @@ void    getFileContentAndInitElfStruct(char *path) {
     char        *file;
     struct stat stat;
     int         fd;
+
     t_elf       *elf;
 
     fd = 0;
@@ -31,19 +32,25 @@ void    getFileContentAndInitElfStruct(char *path) {
     file = mmap(NULL, stat.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
     close(fd);
     elf->size = stat.st_size;
+    if (file[0] != 0x7f || file[1] != 'E' || file[2] != 'L' || file[3] != 'F') {
+        return ;
+    }
     if (file[4] == ELFCLASS32) {
         elf->fileContent32 = (Elf32_Ehdr*) file;
-        elf->arch = 32;
-    } else {
+        elf->arch = B32;
+    } else if (file[4] == ELFCLASS64) {
         elf->fileContent64 = (Elf64_Ehdr*) file;
-        elf->arch = 64;
+        elf->arch = B64;
     }
 }
 
 
 int     main(int ac, char **av) {
     if (ac < 2) {
-        return 0;
+        getFileContentAndInitElfStruct("a.out");
+    } else {
+        for (int i = 1; i < ac; i++)
+        getFileContentAndInitElfStruct(av[i]);
     }
-    getFileContentAndInitElfStruct(av[1]);
+    printf("%s\n", getFileStruct()->arch);
 }
